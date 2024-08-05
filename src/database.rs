0178a9df03e8c8ea67ab5ebf6f63b06775473ef1
Andrew Laird <lairdandrew11@gmail.com>
2024-08-04 22:56:23 -0700

    websockets working, need to push

--- src/database.rs
+++ src/database.rs
@@ -1,45 +1,11 @@
-use crate::chess_structs::ChessState;
 use crate::game::Game;
 use std::error::Error;
 use tokio::fs::{File, OpenOptions};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
 
-pub async fn save_board(name: String, board: ChessState) -> Result<(), Box<dyn std::error::Error>> {
-    let directory = String::from("games/");
-    let full_path = directory + &name + ".pchess";
-    println!("Saving: \n{}", board.chessboard.display_board_str());
-    println!("Saving to: {}", full_path);
-
-    let mut file = File::create(&full_path).await?;
-
-    // Convert the board to a JSON string
-    let j = serde_json::to_string(&board)?;
-
-    // Write the JSON string to the file
-    file.write_all(j.as_bytes()).await?;
-    file.flush().await?; // Explicitly flush the file buffer
-
-    Ok(())
-}
-
-pub async fn load_board(name: &String) -> Result<ChessState, Box<dyn std::error::Error>> {
-    let directory = String::from("games/");
-    let full_path = directory + &name + ".pchess";
-    let mut file = File::open(&full_path).await?;
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).await?;
-
-    // Deserialize the JSON string to a ChessState
-    let board: ChessState = serde_json::from_str(&contents)?;
-
-    Ok(board)
-}
-
 pub async fn save_game(game: Game) -> Result<(), Box<dyn std::error::Error>> {
     let directory = String::from("games/");
     let full_path = directory + &game.name + ".pchess";
-    println!("Saving game to: {}", full_path);
     let mut file = File::create(&full_path).await?;
 
     // Convert the board to a JSON string
